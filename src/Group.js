a9fc4193560f50cd4c27d8905cfed2f9b5941841
AlePontes <alexandrelopespontes@gmail.com>
2019-05-16 13:07:37 -0300

    Add Group Component

---
+++ src/Group.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Group.css';
+import Item from './Item.js'
+import Separator from './Separator.js'
+
+function Group() {
+    return (
+        <div className="Group">
+            <h2 className="Title">Lorem</h2>
+            <Item />
+            <Separator />
+            <Item />
+            <Separator />
+            <Item />
+            <Separator />
+            <Item />
+            <Separator />
+            <Item />
+            <Separator />
+            <Item />
+            <Separator />
+            <Item />
+            <Separator />
+            <Item />
+            <Separator />
+            <Item />
+            <Separator />
+            <Item />
+        </div>
+    );
+}
+
+export default Group;
