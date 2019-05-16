b2b1d4f849ded9b84e234c29405d3b9c956fcd39
AlePontes <alexandrelopespontes@gmail.com>
2019-05-16 13:07:52 -0300

    Add Item Component

---
+++ src/Item.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Item.css';
+// import Group from './Group.css'
+
+function App() {
+    return (
+        <div className="Item">
+            <div className="Image"></div>
+            <div className="Text">
+                <p className="Name">Alexandre Pontes</p>
+                <p className="Email">alexandrelopespontes@gmail.com</p>
+            </div>
+        </div>
+    );
+}
+
+export default App;
